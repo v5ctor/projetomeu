85f526dd4abfaa819f53e69c2c5802e7acd5c61a
v5ctor <victorbitt00@gmail.com>
2020-06-09 15:14:48 -0300

    Atualizado no flake8 os programas

--- seraqvai.py
+++ seraqvai.py
@@ -4,6 +4,8 @@ import requests
 def buscar_avatar(usuario):
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
-    return resp.json()['avatar_url'],resp.json()['id']
+    return resp.json()['avatar_url'], resp.json()['id']
+
+
 if __name__ == '__main__':
     print(buscar_avatar('v5ctor'))
