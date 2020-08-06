697fd6cc4503aed4cdbd4aadc78a97a9c6b8733e
v5ctor <victorbitt00@gmail.com>
2020-08-06 14:40:42 -0300

    Tentativa de fazer funcionar

--- projetomeu/db.py
+++ projetomeu/db.py
@@ -1,6 +1,6 @@
 class Sessao:
-    contador =0
-    usuarios=[]
+    contador = 0
+    usuarios = []
     def salvar(self, usuario):
         Sessao.contador +=1
         usuario.id = Sessao.contador
